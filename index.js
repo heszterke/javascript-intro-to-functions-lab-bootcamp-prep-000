0f483f8f9e84fb2df4c8a4cbf1ff8a199ea8220c
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-03-31 16:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
 }
 
-function sayHiToGrandma(string){
-  if (string.toUpperCase())
+{  if (string.toUpperCase())
   return "YES INDEED!"
 }
