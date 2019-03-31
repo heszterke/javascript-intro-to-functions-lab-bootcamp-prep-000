4fb5d75c5d4be6190dc55e5c994de7c738f21a8f
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-03-31 15:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function whisper(string){
   return string.toLowerCase()
 }
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
+
 function logShout(string){
   uppercase = string.toUpperCase()
   console.log("uppercase")
 }
-logShout("Hello!")
+
